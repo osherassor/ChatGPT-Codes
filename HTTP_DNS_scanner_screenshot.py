9448533f2db897c4e737e9e219025a91e8c7cb92
osherassor <46926530+osherassor@users.noreply.github.com>
2022-12-19 22:51:01 +0200

    Update HTTP_DNS_scanner_screenshot.py

--- HTTP_DNS_scanner_screenshot.py
+++ HTTP_DNS_scanner_screenshot.py
@@ -3,17 +3,21 @@ import socket
 import requests
 import selenium
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import ipaddress
+import threading
+
 
 # This function takes an IP address as input and returns a list of hostnames
 # for that IP address by querying a few different sources
 def get_reverse_dns(ip):
-    hostnames = []
+    hostnames = set()  # Use a set to store the hostnames
 
     # Query the first source
     try:
         hostname = socket.gethostbyaddr(ip)[0]
         if "." in hostname:  # Validate that the result is a domain name
-            hostnames.append(hostname)
+            hostnames.add(hostname)  # Add the hostname to the set
     except:
         pass
 
@@ -21,17 +25,22 @@ def get_reverse_dns(ip):
     try:
         hostname = socket.gethostbyname(ip)
         if "." in hostname:  # Validate that the result is a domain name
-            hostnames.append(hostname)
+            hostnames.add(hostname)  # Add the hostname to the set
     except:
         pass
 
     # Query the third source
     try:
         response = requests.get(f"http://api.hackertarget.com/reverseiplookup/?q={ip}")
-        hostnames += response.text.split("\n")
+        results = response.text.split("\n")
+        for result in results:
+            if "." in result:
+                hostnames.add(result)  # Add the hostname to the set
     except:
         pass
-
+    # Convert the set to a list and return the list of hostnames
+    hostnames = list(hostnames)
+    print(hostnames)
     return hostnames
 
 # This function takes an IP address, a port, and a protocol as input and returns
@@ -47,11 +56,8 @@ def website_accessible(ip, port, protocol):
 
 # This function takes an IP address, a port, and a protocol as input and takes
 # a screenshot of the website at that IP and port using the specified protocol
-def take_screenshot(ip, port, protocol):
-    # Set up Selenium
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(chrome_options=options)
+def take_screenshot(ip, port, protocol, response_code):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
     # Navigate to the website and take the screenshot
     url = f"{protocol}://{ip}:{port}"
@@ -60,35 +66,70 @@ def take_screenshot(ip, port, protocol):
     driver.close()
 
     # Save the screenshot to the specified directory
-    filename = f"{ip}_{port}_{protocol}.png"
+    filename = f"{ip}_{port}_{protocol}_{response_code}.png"
     path = r"c:\tmp\screenshots"
     if not os.path.exists(path):
         os.makedirs(path)
     with open(os.path.join(path, filename), "wb") as f:
         f.write(screenshot)
 
+def check_accessibility(url, accessible_urls):
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code in [200, 301, 302, 403]:
+            print(f"\033[32m{url} is accessible (status code: {response.status_code})\033[0m")
+            # Add the URL to the set of accessible URLs
+            accessible_urls.add(url)
+            try:
+                ip, port, protocol = url.split("://")[1].split(":")
+                take_screenshot(ip, port, protocol, response.status_code)
+            except:
+                pass
+        else:
+            print(f"\033[31m{url} is not accessible (status code: {response.status_code})\033[0m")
+    except:
+        print(f"\033[31m{url} is not accessible\033[0m")
+
 # This is the main function that puts everything together
 def main():
-    # Read the list of IP addresses from the file
+    # Read the list of IP addresses or network ranges from the file
     with open("ips.txt", "r") as f:
         ips = f.read().split("\n")
 
-    # For each IP address, get the list of hostnames and check accessibility
-    # using different protocols and ports
+    # Create a set to store the accessible URLs
+    accessible_urls = set()
+
+    # For each IP address or network range, get the list of hostnames and check
+    # accessibility using different protocols and ports
     for ip in ips:
-        hostnames = get_reverse_dns(ip)
-        hostnames.append(ip)  # Add the IP itself to the hostnames list
-        for hostname in hostnames:
-            for port in [80, 443, 8080, 8443]:
-                for protocol in ["http", "https"]:
-                    if website_accessible(hostname, port, protocol):
-                        print(f"{hostname}:{port} ({protocol}) is accessible")
-                        try:
-                            take_screenshot(hostname, port, protocol)
-                        except:
-                            print(f"Failed to take screenshot of {hostname}:{port} ({protocol})")
-                    else:
-                        print(f"{hostname}:{port} ({protocol}) is not accessible")
+        # If the IP is a network range, generate a list of all the host IP addresses in the range
+        if "/" in ip:
+            ip_range = ipaddress.ip_network(ip)
+            ip_list = [str(ip) for ip in ip_range.hosts()]
+        else:
+            ip_list = [ip]
+
+        for ip in ip_list:
+            hostnames = get_reverse_dns(ip)
+            hostnames.append(ip)  # Add the IP itself to the hostnames list
+            threads = []
+            for hostname in hostnames:
+                for port in [80, 443, 8080, 8443]:
+                    for protocol in ["http", "https"]:
+                        url = f"{protocol}://{hostname}:{port}"
+                        # Create a new thread to check the accessibility of the website
+                        t = threading.Thread(target=check_accessibility, args=(url, accessible_urls))
+                        threads.append(t)
+                        t.start()
+            # Wait for all the threads to complete
+            for t in threads:
+                t.join()
+
+    # Convert the set to a list and print the list of accessible URLs
+    accessible_urls = list(accessible_urls)
+    print("\nAccessible URLs:")
+    for url in accessible_urls:
+        print(url)
 
 if __name__ == "__main__":
     main()
